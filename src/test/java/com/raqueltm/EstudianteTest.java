e0ca60d6a0abef3f09b8a30a60ed4c836a7de7a5
you@example.com <you@example.com>
2024-05-03 12:31:25 +0200

    Cooreccion

--- src/test/java/com/raqueltm/EstudianteTest.java
+++ src/test/java/com/raqueltm/EstudianteTest.java
@@ -1,3 +1,5 @@
+package com.raqueltm;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
